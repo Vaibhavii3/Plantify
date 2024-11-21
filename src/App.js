8d3e57e472165fbe7b78e64a57b9e7601d6b53cf
Vaibhavi <vaibhavichaudhari304@gmail.com>
2024-11-21 23:09:05 +0530

    added Auth page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import LandingPage from "./components/LandingPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuthPage from "./components/AuthPage";
 
 const App = () => {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </Router>
+    
   );
 }
 
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+
+    const Navigate = useNavigate();
     return (
         <div className="bg-green-100 min-h-screen flex flex-col items-center justify-center">
             <h1 className="text-5xl font-bold text-green-600">
@@ -9,7 +12,7 @@ const LandingPage = () => {
             <p className="mt-4 text-lg text-gray-700">
                 Connect with nature lovers, share ideas, and grow together.
             </p>
-            <button className="mt-6 px-6 py-3 bg-green-500 text-white rounded-lg shadow-lg hover:bg-green-600">
+            <button onClick={() => Navigate('/auth')} className="mt-6 px-6 py-3 bg-green-500 text-white rounded-lg shadow-lg hover:bg-green-600">
                 Get Started
             </button>
         </div>
